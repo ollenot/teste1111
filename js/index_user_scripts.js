76a1c3270a62099c62eb996fa51fdba2d1ff03a8
ollenot <ollenot@gmail.com>
2014-05-30 17:56:02 -0300

    oioi

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -8,6 +8,29 @@
  {
     
     
- }
+         $(".uib_w_10").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_15"));  
+        });
+        
+        $("#btnSair").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_15"));  
+        });
+}
  $(document).ready(register_event_handlers);
 })();
