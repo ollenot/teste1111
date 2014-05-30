052e0777fb95a8c829289dcf678b68c7ac0a992b
ollenot <ollenot@gmail.com>
2014-05-30 18:05:45 -0300

    rtrrr

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -31,6 +31,17 @@
         
          uib_sb.toggle_sidebar($(".uib_w_15"));  
         });
+        $(".uib_w_17").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_15"));  
+        });
 }
  $(document).ready(register_event_handlers);
 })();
